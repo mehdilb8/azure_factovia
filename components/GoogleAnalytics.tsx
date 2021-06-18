36a06c6b459ead02bc1bc00305f201cdf5c256bb
Louis Charavner <louis.charavner@viseo.com>
2021-06-18 15:29:05 +0200

    useless modification but i need trigger github action

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -5,16 +5,16 @@ export default function GoogleAnalytics() {
     const analytics = () => {
         if (typeof window !== 'undefined') {
             //@ts-ignore
-            window.dataLayer = window.dataLayer || [];
+            window.dataLayer = window.dataLayer || []
             //@ts-ignore
-            function gtag() { dataLayer.push(arguments); }
+            function gtag() { dataLayer.push(arguments) }
             //@ts-ignore
-            gtag('js', new Date());
+            gtag('js', new Date())
 
             //@ts-ignore
-            gtag('config', 'UA-199420266-1');
+            gtag('config', 'UA-199420266-1')
         }
-        return null;
+        return null
     }
 
     return <>
