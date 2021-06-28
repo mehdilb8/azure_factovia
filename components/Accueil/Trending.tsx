9736239d73925454930c8bcc501798f0e4095d76
Louis Charavner <louis.charavner@viseo.com>
2021-06-28 17:42:51 +0200

    responsive trending

--- components/Accueil/Trending.tsx
+++ components/Accueil/Trending.tsx
@@ -8,15 +8,15 @@ export default function Trending(props: TrendingProps) {
     return <>
         <AccueilContainer >
             <>
-                <div className="py-12">
+                <div className="py-12 lg:py-24">
                     <h2 className="text-primaryDark text-4xl font-bold mb-8">{i18n.t("trending.title")}</h2>
-                    <div className="flex">
-                        <div className="w-1/2">
+                    <div className="flex flex-wrap">
+                        <div className="w-full lg:w-1/2 mb-6 lg:mb-0">
                             <p className="text-gray-500 text-sm mb-2">{new Date(props.blog.date || Date.now()).toLocaleDateString()}</p>
                             <h2 className="text-3xl my-2 cursor-pointer">{props.blog.title}</h2>
                             <p>{props.blog.description}</p>
                         </div>
-                        <div className="w-1/2">
+                        <div className="w-full lg:w-1/2">
                             <Link href={"/" + i18n.activeLocale + "/blog/" + props.blog.slug}>
                                 <a>
                                     <img alt={props.blog.title} className="blog-highlight-thumb w-full object-cover rounded-sm shadow-md transition-transform transform hover:translate-x-2 hover:-translate-y-2" src={props.blog.thumb || `https://picsum.photos/seed/${props.blog.title}/1600`} />
