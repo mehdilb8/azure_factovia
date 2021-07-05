5f245daeb9f7d62d3a30e3c9438a2e0ca170036a
Charavner Louis <Distil62@users.noreply.github.com>
2021-07-05 17:28:40 +0200

    fix issue in offer

--- components/Offers/OfferHero.tsx
+++ components/Offers/OfferHero.tsx
@@ -3,23 +3,25 @@ import AccueilContainer from "../Accueil/AccueilContainer";
 
 export default function OfferHero() {
     return <AccueilContainer>
-        <p>WORK IN PROGRESS !!!</p>
-        <div className="flex flex-wrap">
-            <div className="w-1/3">
-                <div className="lg:pr-12">
+        <>
+            <p>WORK IN PROGRESS !!!</p>
+            <div className="flex flex-wrap">
+                <div className="w-1/3">
+                    <div className="lg:pr-12">
 
+                    </div>
+                </div>
+                <div className="w-1/3">
+                    <div className="lg:px-6">
+
+                    </div>
+                </div>
+                <div className="w-1/3">
+                    <div className="lg:pl-12">
+
+                    </div>
                 </div>
             </div>
-            <div className="w-1/3">
-                <div className="lg:px-6">
-
-                </div>
-            </div>
-            <div className="w-1/3">
-                <div className="lg:pl-12">
-
-                </div>
-            </div>
-        </div>
+        </>
     </AccueilContainer>
 }
