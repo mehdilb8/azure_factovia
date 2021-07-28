261102d137ece483c70482ac3ff6c9bfcadc7d31
Charavner Louis <Distil62@users.noreply.github.com>
2021-07-28 17:47:18 +0200

    keepScroll position WIP. Somes issues are always presents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import I18n from '../lib/i18n'
 import RGPD from '../components/RGPD'
+import keepScroll from '../lib/keepScroll'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
 
   if (!pageProps.lngDict || !pageProps.lng) {
     console.warn("You should send dictionnary of lang and lang herself")
   }
+
+  useEffect(() => {
+    const clearKeppScroll = keepScroll()
+
+    return () => {
+      clearKeppScroll()
+    }
+
+  }, [router])
 
   return <>
     <I18n lngDict={pageProps.lngDict} locale={pageProps.lng}>
