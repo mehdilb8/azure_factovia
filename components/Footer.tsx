59fbcc50daabf1480de17e90bc259bc8e37aa6a2
Charavner Louis <Distil62@users.noreply.github.com>
2021-08-06 17:27:04 +0200

    fix footer issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,16 +14,18 @@ export default function Footer() {
         <div className="flex flex-wrap justify-center md:justify-around space-y-8 md:space-y-0">
             <div className="w-full md:w-1/4 flex md:block justify-center">
                 <div className="w-48 md:w-auto">
-                    <div className="h-16 mb-2">
-                        <Link href={"/" + i18n.activeLocale}>
-                            <a>
-                                <LazyElement>
-                                    <img src={LOGO_GREEN_URL} alt="Factovia Logo" className="h-full" />
-                                </LazyElement>
-                            </a>
-                        </Link>
+                    <div className="flex items-center">
+                        <div className="h-16 mb-2 mr-2">
+                            <Link href={"/" + i18n.activeLocale}>
+                                <a>
+                                    <LazyElement>
+                                        <img src={LOGO_GREEN_URL} alt="Factovia Logo" className="h-full" />
+                                    </LazyElement>
+                                </a>
+                            </Link>
+                        </div>
+                        <p className="text-secondary">{i18n.t("footer.identity.slogan")}</p>
                     </div>
-                    <p className="text-secondary">{i18n.t("footer.identity.slogan")}</p>
                     <p className="text-secondary">{i18n.t("footer.identity.copyright")} {copyrightDate}</p>
                 </div>
             </div>
