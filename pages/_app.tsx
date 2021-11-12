3059464da1a189c984239eb4187ec285b8604401
Louis Charavner <distil47@gmail.com>
2021-11-12 12:56:26 +0100

    remove verification balise of google search

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
-import Head from 'next/head'
 import I18n from '../lib/i18n'
 import RGPD from '../components/RGPD'
 // import Bot from '../components/Bot'
@@ -11,9 +10,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }
 
   return <>
-    <Head>
-      <meta name="google-site-verification" content="cW6LqgAqLcs0BufZRldQesFHz4W-qIfkGadlcQ5AoMk" />
-    </Head>
     <I18n lngDict={pageProps.lngDict} locale={pageProps.lng}>
       <div>
         <RGPD />
